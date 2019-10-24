d2f3b0718fe005e71df65095f24cc1c9bcab663b
Unbinilium <Unbinilium@users.noreply.github.com>
2019-10-25 02:41:59 +0800

    Switch v0.5.2
    Refine text color and code

--- switch.ps1
+++ switch.ps1
@@ -1,42 +1,25 @@
-#Switch v0.5.1 written by Unbinilium
+#Switch v0.5.2 written by Unbinilium
 
 #Check if Administrator
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
 
-#Set Error Action and Start up
+#Start Up with Base64 Encoded
+$Start_Up = "V3JpdGUtSG9zdCAtRm9yZWdyb3VuZENvbG9yIEdyYXkgIi0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIg0KV3JpdGUtSG9zdCAtRm9yZWdyb3VuZENvbG9yIEdyYXkgIiAgICAgICAgICAgICAgICoqKioiDQpXcml0ZS1Ib3N0IC1Gb3JlZ3JvdW5kQ29sb3IgR3JheSAiICAgICAgICAgICAgKioqKioqKiogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAqKiINCldyaXRlLUhvc3QgLUZvcmVncm91bmRDb2xvciBHcmF5ICIgICAgICAgICAgICoqKiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICoqIg0KV3JpdGUtSG9zdCAtRm9yZWdyb3VuZENvbG9yIEdyYXkgIiAgICAgICAgICAgICAqKiogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgKioiDQpXcml0ZS1Ib3N0IC1Gb3JlZ3JvdW5kQ29sb3IgR3JheSAiICAgICAgICAgICAgICAgKioqICAgICAgICAgICAgICAgICAgICoqICAgICoqICAgICAgICAgICAqKiINCldyaXRlLUhvc3QgLUZvcmVncm91bmRDb2xvciBHcmF5ICIgICAgICAgICAgICAgICAgICoqKiAgICAgICAgICAgICAgICAgICAgICAgKiogICAgKioqKioqICoqKioqKioqIg0KV3JpdGUtSG9zdCAtRm9yZWdyb3VuZENvbG9yIEdyYXkgIiAgICAgICAgICAgICAgICAgICAqKiogICAgICAqICAgICoqKiAqKiAqKioqKioqKiAqKiAgICAgKiogICAgKioiDQpXcml0ZS1Ib3N0IC1Gb3JlZ3JvdW5kQ29sb3IgR3JheSAiICAgICAgICAgICAqKioqKioqKiogICoqKiAgKioqICAqKiogICoqICAgICoqICAgICoqICAgICAqKiAgICAqKiINCldyaXRlLUhvc3QgLUZvcmVncm91bmRDb2xvciBHcmF5ICIgICAgICAgICAgICAgKioqKiogICAgICAqKiogICAqKiogICAgKiogICAgKioqKiogKioqKioqICoqICAgICoqIg0KV3JpdGUtSG9zdCAtRm9yZWdyb3VuZENvbG9yIEdyYXkgIi0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIg0KV3JpdGUtSG9zdCAtRm9yZWdyb3VuZENvbG9yIEdyYXkgIiAgICAgICAgICAgICAgICAgU3dpdGNoIEROUyBNb2RpZmllciBXcml0dGVuIGJ5IFVuYmluaWxpdW0iDQpXcml0ZS1Ib3N0IC1Gb3JlZ3JvdW5kQ29sb3IgR3JheSAiLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0i"
+
+#Set Error Action and Display Start Up
 $ErrorActionPreference = "Continue"
-Write-Host "-----------------------------------------------------------------"
-Write-Host "          ****"
-Write-Host "        ********"
-Write-Host "      ***                                          **"
-Write-Host "        ***                                        **"
-Write-Host "          ***                   **    **           **"
-Write-Host "            ***                       **    ****** ********"
-Write-Host "              ***      *    *** ** ******** **     **    **"
-Write-Host "      *********  ***  ***  ***  **    **    **     **    **"
-Write-Host "        *****      ***   ***    **    ***** ****** **    **"
-Write-Host "-----------------------------------------------------------------"
-Write-Host "            Switch DNS Modifier Written by Unbinilium"
-Write-Host "-----------------------------------------------------------------"
+$Start_Up_TMP = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp', 'StartUp.ps1' } -PassThru
+[System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($Start_Up)) | Out-File -FilePath "$Start_Up_TMP"
+powershell "$Start_Up_TMP"
+Remove-Item -Path "$Start_Up_TMP"
 
 #DNS List with Base64 Encoded
-#114DNS/"114.114.114.114","114.114.115.115"
-#Google/"8.8.8.8","8.8.4.4"
-#Cloudflare/"1.1.1.1","1.0.0.1"
-#Aliyun/"223.5.5.5","223.6.6.6"
-#CNNIC/"1.2.4.8","210.2.4.8"
-#Baidu/"180.76.76.76"
-#Dnspod/"119.29.29.29","182.254.116.116"
-#OpenDNS/"208.67.222.222","208.67.220.220"
-#Microsoft/"4.2.2.1","4.2.2.2"
-#Custom/
-#Default/
 $DNS_List = "IzExNEROUy8iMTE0LjExNC4xMTQuMTE0IiwiMTE0LjExNC4xMTUuMTE1Ig0KI0dvb2dsZS8iOC44LjguOCIsIjguOC40LjQiDQojQ2xvdWRmbGFyZS8iMS4xLjEuMSIsIjEuMC4wLjEiDQojQWxpeXVuLyIyMjMuNS41LjUiLCIyMjMuNi42LjYiDQojQ05OSUMvIjEuMi40LjgiLCIyMTAuMi40LjgiDQojQmFpZHUvIjE4MC43Ni43Ni43NiINCiNEbnNwb2QvIjExOS4yOS4yOS4yOSIsIjE4Mi4yNTQuMTE2LjExNiINCiNPcGVuRE5TLyIyMDguNjcuMjIyLjIyMiIsIjIwOC42Ny4yMjAuMjIwIg0KI01pY3Jvc29mdC8iNC4yLjIuMSIsIjQuMi4yLjIiDQojQ3VzdG9tLw0KI0RlZmF1bHQv"
 
-#Generate DNS List TMP Path and decode DNS List
+#Generate DNS List TMP Path and Decode DNS List
 $DNS_List_TMP = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp', 'DnsList' } -PassThru
 [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($DNS_List)) | Out-File -FilePath "$DNS_List_TMP"
 
@@ -48,7 +31,7 @@ foreach($DNS_Provider_Name in Get-Content -Path "$DNS_List_TMP") {
    Out-File -FilePath "$DNS_Provider_Name_TMP" -InputObject "$DNS_Provider_Name" -Append -NoNewline
 }
 $DNS_Provider_Name = (Get-Content -Path "$DNS_Provider_Name_TMP").substring(2)
-Write-Host -Object "$DNS_Provider_Name"
+Write-Host -ForegroundColor Gray -Object "$DNS_Provider_Name"
 $DNS_Provider = Read-Host -Prompt 'Please Enter DNS Provider Name to Process (Use " " to spilt)'
 
 #Extract DNS Server from DNS List TMP
@@ -58,29 +41,31 @@ foreach ($DNS in $DNS_Provider.Split(" ")) {
         $Custom_DNS_Address = Read-Host -Prompt 'Please Enter Custome DNS Address (Use "," to spilt)'
         $Custom_DNS_Address = $Custom_DNS_Address.replace(',','","')
         Out-File -FilePath "$DNS_Server_TMP" -InputObject "`"$Custom_DNS_Address`"," -Append -NoNewline
-    } else {
+    }
+    else {
         $DNS_Address = (Select-String -Path $DNS_List_TMP -Pattern "$DNS") -split "/" | Select-Object -Skip 1 -First 1
         Out-File -FilePath "$DNS_Server_TMP" -InputObject "$DNS_Address," -Append -NoNewline
     }
 }
 $DNS_Server = (Get-Content -Path "$DNS_Server_TMP" -Raw).TrimEnd(',')
 
-#Generate Network Adapter TMP Path and extract Net Adapter Index as table
+#Generate Network Adapter TMP Path and Extract Net Adapter Index as table
 $Net_Adapter_TMP = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp', 'NetAdapter' } -PassThru
 Get-NetAdapter | Select-Object -Property "ifIndex" | Format-Table -AutoSize | Out-File -FilePath "$Net_Adapter_TMP"
 $Net_Adapter_Index = (Get-Content -Path "$Net_Adapter_TMP") -creplace "ifIndex", "" -creplace "-", "" | ForEach-Object {$_.Trim()} | Where-Object { $_ } | Sort-Object
 
 #Execute DNS change for each Network Adapter
-$Execute_DNS_TMP = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp', 'ps1' } -PassThru
+$Execute_DNS_TMP = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp', 'ExecuteDNS.ps1' } -PassThru
 if ($DNS_Provider | Select-String -Pattern 'Default') {
     foreach ($Net_Adapter in $Net_Adapter_Index) {
         Set-DnsClientServerAddress -InterfaceIndex "$Net_Adapter" -ResetServerAddresses
     }
-} else {
+}
+else {
     foreach ($Net_Adapter in $Net_Adapter_Index) {
         Out-File -FilePath "$Execute_DNS_TMP" -InputObject "Set-DnsClientServerAddress -InterfaceIndex `"$Net_Adapter`" -ServerAddresses ($DNS_Server)" -Append
     }
-    powershell $Execute_DNS_TMP
+    powershell "$Execute_DNS_TMP"
     Remove-Item -Path "$Execute_DNS_TMP"
 }
 
@@ -90,5 +75,9 @@ Remove-Item -Path "$DNS_Provider_Name_TMP"
 Remove-Item -Path "$DNS_Server_TMP"
 Remove-Item -Path "$Net_Adapter_TMP"
 
+#Show Results
+Get-DnsClientServerAddress
+
 #Press Enter to exit
 Read-Host -Prompt 'DNS Change Execution Finished, Please Press Enter to Exit'
+exit
