fd3b8d328c4a82125aa85481a0739e3085bde522
Unbinilium <Unbinilium@users.noreply.github.com>
2019-10-24 10:20:40 +0800

    Switch Test ver-0.3
    1. Optimize data structure

--- switch.ps1
+++ switch.ps1
@@ -1,4 +1,4 @@
-#Switch v0.2 written by Unbinilium
+#Switch v0.3 written by Unbinilium
 
 #Check if Administrator
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -21,10 +21,6 @@ Write-Host "        *****      ***   ***    **    ***** ****** **    **"
 Write-Host "-----------------------------------------------------------------"
 Write-Host "            Switch DNS Modifier Written by Unbinilium"
 Write-Host "-----------------------------------------------------------------"
-#Specialize DNS Provider
-Write-Host "Supported Dns Providers:"
-Write-Host "114DNS, Google, Cloudflare, Aliyun, CNNIC, Baidu, Dnspod, OpenDNS, Microsoft, Custom"
-$DNS_Provider = Read-Host -Prompt 'Please Enter DNS Provider name to process (Use " " to spilt)'
 
 #DNS List with Base64 Encoded
 #114DNS/"114.114.114.114","114.114.115.115"
@@ -34,13 +30,26 @@ $DNS_Provider = Read-Host -Prompt 'Please Enter DNS Provider name to process (Us
 #CNNIC/"1.2.4.8","210.2.4.8"
 #Baidu/"180.76.76.76"
 #Dnspod/"119.29.29.29","182.254.116.116"
-#OpenDNS/"208.67.220.220","208.67.222.222"
+#OpenDNS/"208.67.222.222","208.67.220.220"
 #Microsoft/"4.2.2.1","4.2.2.2"
-$DNS_List = "IzExNEROUy8iMTE0LjExNC4xMTQuMTE0IiwiMTE0LjExNC4xMTUuMTE1IgojR29vZ2xlLyI4LjguOC44IiwiOC44LjQuNCIKI0Nsb3VkZmxhcmUvIjEuMS4xLjEiLCIxLjAuMC4xIgojQWxpeXVuLyIyMjMuNS41LjUiLCIyMjMuNi42LjYiCiNDTk5JQy8iMS4yLjQuOCIsIjIxMC4yLjQuOCIKI0JhaWR1LyIxODAuNzYuNzYuNzYiCiNEbnNwb2QvIjExOS4yOS4yOS4yOSIsIjE4Mi4yNTQuMTE2LjExNiIKI09wZW5ETlMvIjIwOC42Ny4yMjAuMjIwIiwiMjA4LjY3LjIyMi4yMjIiCiNNaWNyb3NvZnQvIjQuMi4yLjEiLCI0LjIuMi4yIg=="
+#Custom/
+#Default/
+$DNS_List = "IzExNEROUy8iMTE0LjExNC4xMTQuMTE0IiwiMTE0LjExNC4xMTUuMTE1Ig0KI0dvb2dsZS8iOC44LjguOCIsIjguOC40LjQiDQojQ2xvdWRmbGFyZS8iMS4xLjEuMSIsIjEuMC4wLjEiDQojQWxpeXVuLyIyMjMuNS41LjUiLCIyMjMuNi42LjYiDQojQ05OSUMvIjEuMi40LjgiLCIyMTAuMi40LjgiDQojQmFpZHUvIjE4MC43Ni43Ni43NiINCiNEbnNwb2QvIjExOS4yOS4yOS4yOSIsIjE4Mi4yNTQuMTE2LjExNiINCiNPcGVuRE5TLyIyMDguNjcuMjIyLjIyMiIsIjIwOC42Ny4yMjAuMjIwIg0KI01pY3Jvc29mdC8iNC4yLjIuMSIsIjQuMi4yLjIiDQojQ3VzdG9tLw0KI0RlZmF1bHQv"
 
 #Generate DNS List TMP Path and decode DNS List
 $DNS_List_TMP = [System.IO.Path]::GetTempFileName()
 [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($DNS_List)) | Out-File -FilePath "$DNS_List_TMP"
+
+#Specialize DNS Provider
+Write-Host "Supported DNS Providers:"
+$DNS_Provider_Name_TMP = [System.IO.Path]::GetTempFileName()
+foreach($DNS_Provider_Name in Get-Content -Path "$DNS_List_TMP") {
+   $DNS_Provider_Name = (($DNS_Provider_Name) -split "/" | Select-Object -First 1).replace('#',', ')
+   Out-File -FilePath "$DNS_Provider_Name_TMP" -InputObject "$DNS_Provider_Name" -Append -NoNewline
+}
+$DNS_Provider_Name = (Get-Content -Path "$DNS_Provider_Name_TMP").substring(2)
+Write-Host -Object "$DNS_Provider_Name"
+$DNS_Provider = Read-Host -Prompt 'Please Enter DNS Provider name to process (Use " " to spilt)'
 
 #Extract DNS Server from DNS List TMP
 $DNS_Server_TMP = [System.IO.Path]::GetTempFileName()
@@ -58,22 +67,27 @@ foreach ($DNS in $DNS_Provider.Split(" ")) {
     }
 }
 $DNS_Server = (Get-Content -Path "$DNS_Server_TMP").TrimEnd(',')
-Write-Verbose -Verbose $DNS_Server
 
 #Generate Network Adapter TMP Path and extract Net Adapter Index as table
 $Net_Adapter_TMP = [System.IO.Path]::GetTempFileName()
 Get-NetAdapter | Select-Object -Property "ifIndex" | Format-Table -AutoSize | Out-File -FilePath "$Net_Adapter_TMP"
 $Net_Adapter_Index = (Get-Content -Path "$Net_Adapter_TMP") -creplace "ifIndex", "" -creplace "-", "" | ForEach-Object {$_.Trim()} | Where-Object { $_ } | Sort-Object
 
+
 #Execute DNS change for each Network Adapter
 foreach ($Net_Adapter in $Net_Adapter_Index) {
-    Set-DnsClientServerAddress -InterfaceIndex "$Net_Adapter" -ServerAddresses ($DNS_Server) -Validate
+    if ($DNS_Provider | Select-String -Pattern 'Default') {
+        Set-DnsClientServerAddress -InterfaceIndex "$Net_Adapter" -ResetServerAddresses
+    } else {
+        Set-DnsClientServerAddress -InterfaceIndex "$Net_Adapter" -ServerAddresses ($DNS_Server) -Validate
+    }
 }
 
 #Clean up TEMP
 Remove-Item -Path "$DNS_List_TMP"
+Remove-Item -Path "$DNS_Provider_Name_TMP"
 Remove-Item -Path "$DNS_Server_TMP"
 Remove-Item -Path "$Net_Adapter_TMP"
 
 #Press Enter to exit
-Read-Host -Prompt 'DNS Change Execution Succeed, Please Press Enter to EXIT'
+Read-Host -Prompt 'DNS Change Execution Finished, Please Press Enter to EXIT'
